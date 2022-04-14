a286ed1ce7f33af0eb790822f61fa01ad7c3d113
lancarweb <lancarweboffice@gmail.com>
2022-04-14 12:58:09 +0700

    install driver | webdriver manager

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-# mandiri-livin
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
